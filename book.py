4911245fa69ff88e1c196c28133267a78cd72103
Bill JaJa <bostoneboy@gmail.com>
2011-05-17 10:50:32 +0800

    -a

--- book.py
+++ book.py
@@ -94,7 +94,7 @@ def page_chapter_format(page_chapter):
 def book(url_main):
   if re.search(r"http:\/\/book\.qq\.com",url_main):
     website_type = "qqbook"
-    keyword_filename = re.compile(r"<h1>.*ï¿½ï¿½(.*)<\/h1>")
+    keyword_filename = re.compile(r"<h1>.*£º(.*)<\/h1>")
     # no keyword_url_base
     keyword_url_base = ""
     keyword_url = re.compile(r"javascript:opennew\(\'(http\S*shtml).*>(.*)<\/a>")
